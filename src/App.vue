c47033de5ace5b1fff8d4319befc6fe6d83eac95
tranthientam293 <tranthientam2903@gmail.com>
2024-08-06 06:07:45 +0700

    init project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'John Doe',
+      status: false
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status">User is active</p>
+  <p v-else>User is inactive</p>
+</template>
